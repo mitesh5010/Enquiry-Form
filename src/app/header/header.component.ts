1a639aed4d9900113cce099b5c82fc4e695e8346
Mitesh <miteshprajapati6439@gmail.com>
2025-06-05 15:07:32 +0530

    title input make intrective

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed,  signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  title = signal<string>('Untitle form');
+  
+  enterTitle = computed(() => {
+    return this.title().trim();
+  })
+  
+  onPublish() {
+    console.log(
+      this.enterTitle()
+    );
+  }
+  
 }
