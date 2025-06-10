67fbab9bc9f64a3290988856f57ea2aa60b36748
Mitesh <miteshprajapati6439@gmail.com>
2025-06-10 17:21:22 +0530

    Get Form Data into Console with Other Fix

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { FormTitleComponent } from "../form-title/form-title.component";
-import { QuestionsComponent } from "../questions/questions.component";
 import { FormService } from './form.service';
-import { FormsModule } from '@angular/forms';
+import {  FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { QuestionComponent } from "../questions/question/question.component";
+import { Question } from '../questions/question/question.model';
 
 @Component({
   selector: 'app-form',
-  imports: [FormTitleComponent, QuestionsComponent, FormsModule],
+  standalone: true,
+  imports: [FormTitleComponent, FormsModule, QuestionComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -14,10 +16,18 @@ export class FormComponent {
   private id = 1;
 
   constructor(public formService: FormService) {}
+ 
 
   addQuestion() {
     const id = this.id++;
-    this.formService.questions.update(qs => [...qs, { id, text: '', type: 'short-answer' ,answer:'' }]);
+    const newQuestion: Question = {
+    id,
+    text: '',
+    type: 'short-answer',
+    answer: '',
+    required: false,
+    };
+    this.formService.questions.update(qs => [...qs, newQuestion]);
   }
 
   onPublish() {
