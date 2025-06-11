5451d061ce7627531996e6d4bf46e11b14ea83b4
Mitesh <miteshprajapati6439@gmail.com>
2025-06-11 13:21:28 +0530

    Create User-Form Component and set route for Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { HeaderComponent } from "./header/header.component";
 import { FormComponent } from "./form/form.component";
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+import { UserFormComponent } from "./user-form/user-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,FormComponent],
+  imports: [HeaderComponent, FormComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Enquiry-Form';
+
+  private router = inject(Router);
+  currentPath = signal<string>('');
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentPath.set(event.urlAfterRedirects);
+    })
+  }
+
+  isUserRoute = computed(()=> this.currentPath().startsWith('/user-form'));
 }
