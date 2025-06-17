d8d34c990c5fea33f0d9c8012945679ac3179056
Mitesh <miteshprajapati6439@gmail.com>
2025-06-17 13:28:02 +0530

    responses data get into  that form

--- src/app/responses/responses.component.ts
+++ src/app/responses/responses.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { ApiResponse, FormResponse } from './responses.model';
+import { ActivatedRoute } from '@angular/router';
+import { ResponseApiService } from './response-api.service';
 
 @Component({
   selector: 'app-responses',
@@ -7,6 +9,26 @@ import { HeaderComponent } from "../header/header.component";
   templateUrl: './responses.component.html',
   styleUrl: './responses.component.css'
 })
-export class ResponsesComponent {
+export class ResponsesComponent implements OnInit {
+  formResponse!: ApiResponse ;
+  formId!:number;
+  activeTab: 'question' | 'individual' = 'question';
+  totalRes:number=0;
+  totalQue:number=0;
 
+  constructor(private route: ActivatedRoute, private responseApi: ResponseApiService){}
+
+  ngOnInit(): void {
+    this.formId = +this.route.parent?.snapshot.params['id'];
+    this.responseApi.getresponseById(this.formId).subscribe(data => {
+      this.formResponse = data[0];
+      this.totalRes = this.formResponse.response.length;
+      this.totalQue = this.formResponse.response[0].questionAnswer.length;
+    });
+  }
+
+
+  setTab(tab: 'question' | 'individual'){
+    this.activeTab = tab;
+  }
 }
